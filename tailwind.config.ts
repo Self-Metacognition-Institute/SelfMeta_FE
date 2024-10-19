81cfe767f0a274e9be35e8008f98df580751662b
Barış Hakverdi <info@barishakverdi.com>
2024-10-19 15:32:41 +0300

    feat: antd config added, tailwind config edited, global css added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,40 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'min-2xl': { min: "1681px" },
+      'min-xl': { min: "1441px" },
+      'min-lg': { min: "1281px" },
+      'min-md': { min: "1025px" },
+      'min-sm': { min: "769px" },
+      'min-xsm': { min: "577px" },
+      'min-xs': { min: "481px" },
+      '2xl': { max: "1680px" },
+      'xl': { max: "1440px" },
+      'lg': { max: "1280px" },
+      'md': { max: "1024px" },
+      'sm': { max: "768px" },
+      'ssm': { max: "576px" },
+      'xsm': { max: "624px" },
+      'xs': { max: "480px" },
+      '2xs': { max: "375px" },
+    },
+    opacity: generateOpacityRange(1, 100),
+    zIndex: generateZIndexRange(1, 100),
+    transitionDuration: generateTransitionDurationRange(100, 1000),
+    transitionDelay: generateDelayRange(100, 1000),
+    aspectRatio: {
+      auto: 'auto',
+      square: '1 / 1',
+      video: '16 / 9',
+      unset: 'unset',
+    },
+    fontFamily: {
+      sans : ['Archivo', 'Montserrat', 'Liberation Mono', 'Menlo', 'Courier', 'monospace']
+    },
     extend: {
       colors: {
+        primary: "#3896BD",
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
@@ -16,4 +48,45 @@ const config: Config = {
   },
   plugins: [],
 };
+
+// Generate Opacities
+function generateOpacityRange(start: number, end: number) {
+  const opacityRange = {};
+  for (let i = start; i <= end; i++) {
+    // @ts-ignore
+    opacityRange[i] = (i / 100).toString();
+  }
+  return opacityRange;
+}
+
+// Generate z-indexes
+function generateZIndexRange(start: number, end: number) {
+  const zIndexRange = {};
+  for (let i = start; i <= end; i++) {
+    // @ts-ignore
+    zIndexRange[i] = i.toString();
+  }
+  return zIndexRange;
+}
+
+// Generate Transition Durations
+function generateTransitionDurationRange(start: number, end: number) {
+  const transitionDurationRange = {};
+  for (let i = start; i <= end; i+=50) {
+    // @ts-ignore
+    transitionDurationRange[i] = (i + "ms").toString();
+  }
+  return transitionDurationRange;
+}
+
+// Generate Transition Delays
+function generateDelayRange(start: number, end: number) {
+  const delayRange = {};
+  for (let i = start; i <= end; i+=50) {
+    // @ts-ignore
+    delayRange[i] = (i + "ms").toString();
+  }
+  return delayRange;
+}
+
 export default config;
