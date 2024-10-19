af7d972635e44417f290c2d4278bedb452215c2d
Faruk Çakır <farukckr55@gmail.com>
2024-10-19 13:47:02 +0300

    feat: theme config initilaized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 
 import React from 'react';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
-
+import { ConfigProvider } from 'antd';
+import theme from "@/themes/themeConfig"
 const RootLayout = ({ children }: React.PropsWithChildren) => (
   <html lang="en">
     <body>
-      <AntdRegistry>{children}</AntdRegistry>
+      <ConfigProvider theme={theme}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </ConfigProvider>
     </body>
   </html>
 );
