36f4f6665ac7c6c8b4dd8c8510c91e3dbe4655dc
furkanUçukoğlu <56740798+phoenix-hub1@users.noreply.github.com>
2024-11-13 20:39:10 +0300

    design: login page and forgot password page created

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -32,41 +32,11 @@ const page = () => {
                 }
             },
             {
-                fieldKey: "password",
-                renderComponent: SmPasswordInput,
-                componentProps: {
-                    labelClass: "text-sm text-red-500 font-semibold hidden",
-                    inputName: "password",
-                    label: "Password Input",
-                    placeholder: "Şifre",
-                    fullWidth: true,
-                    size: "large"
-                }
-            },
-
-            {
-                fieldKey: "test3",
-                renderComponent: ({ checkboxProps, linkProps }: { checkboxProps: ICheckboxProps; linkProps: LabelLinkProps }) => (
-                    <div className="flex items-center space-x-2">
-                        <SmCheckbox {...checkboxProps} />
-                        <SmLabelLink {...linkProps} />
-                    </div>
-                ),
-                componentProps: {
-                    checkboxProps: {
-                        text: "Beni Hatırla"
-                    },
-                    linkProps: {
-                        text: "Şifrenizi mi unuttunuz?",
-                        href: "/auth/forgot-password" 
-                    }
-                }
-            },
-            {
                 fieldKey: "test4",
                 renderComponent: SmButton,
                 componentProps: {
-                    text: "Giriş Yap",
+                    
+                    text: "Şifre Sıfırlama Bağlantısını Gönder",
                     type: "primary"
                 }
             },
@@ -81,7 +51,7 @@ const page = () => {
         <div className="flex flex-col items-center gap-6">
             <div className="flex flex-col items-center gap-5 sm:gap-3">
                 <Image src="/images/logo.svg" alt="logo" width={129} height={60} className="w-[129px] h-auto" />
-                <h1 className="text-[20px] leading-[1.4] font-medium text-[#262626] text-center sm:text-[16px]">Self Metacognition Institute’a Hoş geldin.</h1>
+                <h1 className="text-[20px] leading-[1.4] font-medium text-[#262626] text-center sm:text-[16px]">Şifreni sıfırlamak için kayıtlı e-postanı gir.</h1>
             </div>
             <DynamicForm fields={formState.fields} initialData={formState.initialData} wrapperClass="flex flex-col gap-4" />
             <Text className="text-[12px] text-center">
